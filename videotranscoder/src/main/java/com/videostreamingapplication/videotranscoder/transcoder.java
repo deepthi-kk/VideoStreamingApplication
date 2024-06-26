8265ecd9b7adafa944ec2bec078cb9a6751717c7
Chaitanya Deepthi Kaki <chaitanyadeepthikaki@Chaitanyas-Laptop.local>
2024-06-25 23:50:07 -0700

    initial commit

---
+++ videotranscoder/src/main/java/com/videostreamingapplication/videotranscoder/transcoder.java
@@ -0,0 +1,15 @@
+package com.videostreamingapplication.videotranscoder;
+
+/**
+ * Hello world!
+ *
+ */
+public class transcoder 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+
+    }
+}
